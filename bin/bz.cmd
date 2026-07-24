dd06a48ec615059dfa77d330dccfbb01fb4d84df
GetSoftwares <glwjfsk@outlook.com>
2026-07-24 11:48:04 +0800

    change：关闭了 bz 命令行的输出
    由于 bz 命令行在操作时会强制输出操作日志且不能禁用，所以修改了 bz.cmd 来重定向它们的输出

--- bin/bz.cmd
+++ bin/bz.cmd
@@ -1,8 +1,8 @@
 @echo off
 if /I "%PROCESSOR_ARCHITECTURE%" equ "AMD64" (
-    bz.x64.exe %*
+    bz.x64.exe %* 1>NUL
 ) else if /I "%PROCESSOR_ARCHITECTURE%" equ "x86" (
-    bz.x86.exe %*
+    bz.x86.exe %* 1>NUL
 ) else (
-    bz.x86.exe %*
+    bz.x86.exe %* 1>NUL
 )
